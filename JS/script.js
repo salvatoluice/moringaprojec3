c51b7c803c3b92f6a342e9b5998f299608621119
Salvato Luice <salvatoluice5@gmai.com>
2022-07-16 13:57:22 +0300

    Add products section

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,32 @@ $(document).ready(function(){
         $('ul').toggleClass('show')
     });
 });
+
+
+let shade1 = 510;
+let shade2 = 308;
+let shade3 = 486;
+let shade4 = 572; 
+
+
+
+const obj = {
+    sum: function(){
+        return shade1 + shade2 + shade3 + shade4;
+    },
+    total: function(){
+        return "Your total output per day is " + this.sum() + " litres";
+    },
+}
+
+const myObject = {
+    shadeA:"1. Your Production in Shade A is " + shade1 +  " litres per day",
+    shadeB:"2. Your Production in Shade A is " + shade2 +  " litres per day",
+    shadeC: "3. Your Production in Shade C is " + shade3 + " litres per day",
+    shadeD: "4. Your Production in Shade D is " + shade4 + " litres per day",
+    total: function() {
+      return this.shadeA + "<br />" + this.shadeB + "<br />" + this.shadeC + "<br />" + this.shadeD;
+    }
+  }
+  document.getElementById("demo").innerHTML = myObject.total(); 
+  document.getElementById("total").innerHTML = obj.total();
